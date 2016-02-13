86e096f291bfddf489ac1c69f8500664eea3e516
Selestina <selestina51@gmail.com>
2016-02-13 23:27:02 +0400

    puzzle

--- forhtml.js
+++ forhtml.js
@@ -7,8 +7,12 @@ img = document.getElementById('img');
 //    document.getElementById('background').style.backgroundPosition = (e.pageX * -1 / 4) + 'px ' + (e.pageY * -1 / 4) + 'px';
 //  }
 //});
-$("#rows").slider({value:8, min: 4, max: 12, step: 1});
-$("#cols").slider({value:8, min: 4, max: 12, step: 1});
+$("#rows").slider({
+  value:84,
+  min: 12,
+  max: 144,
+  step: 12,
+  slide: function(event,ui){$('#rowscount').html(ui.value)}});
 $("#congratulation").hide();
 function start(file) {
   $('#settings').hide();
@@ -24,13 +28,39 @@ function start(file) {
       zIndex=3;
       //rows = document.getElementById('rows').value;
       //cols = document.getElementById('cols').value;
-      rows= $('#rows').slider('value');
-      cols= $('#cols').slider('value');
-      (document.getElementById('rotation').checked) ? isRotate = true : isRotate = false;    
-      rowsHeight = (img.height/rows);
-      colsWidth = (img.width/cols);
-    //rowsHeight = Math.round(img.height/rows);
-    //colsWidth = Math.round(img.width/cols);
+      //rows= $('#rows').slider('value');
+      if ($('#rows').slider('value')==12){rows=3, cols=4};
+      if ($('#rows').slider('value')==24){rows=4, cols=6};
+      if ($('#rows').slider('value')==36){rows=4, cols=9};
+      if ($('#rows').slider('value')==48){rows=6, cols=8};
+      if ($('#rows').slider('value')==60){rows=6, cols=10};
+      if ($('#rows').slider('value')==72){rows=6, cols=12};
+      if ($('#rows').slider('value')==84){rows=7, cols=12};
+      if ($('#rows').slider('value')==96){rows=8, cols=12};
+      if ($('#rows').slider('value')==108){rows=9, cols=12};
+      if ($('#rows').slider('value')==120){rows=6, cols=20};
+      if ($('#rows').slider('value')==132){rows=11, cols=12};
+      if ($('#rows').slider('value')==144){rows=12, cols=12};
+      //if ($('#rows').slider('value')==160){rows=25, cols=25};
+      //cols= $('#cols').slider('value');
+      (document.getElementById('rotation').checked) ? isRotate = true : isRotate = false;
+      puzzleLogical = ((img.width/cols) < (img.height/rows)) ? img.width/cols : img.height/rows;
+      //puzzleLogical = img.width/cols;
+      imgHeight = puzzleLogical*rows;  
+      imgWidth = puzzleLogical*cols;
+      puzzleOffset=puzzleLogical/100*35;
+      puzzleSize=puzzleLogical/100*170;
+      imgCanvas= document.createElement('canvas');
+      imgCanvas.width=imgWidth;
+      imgCanvas.height=imgHeight;
+      imgCanvasctx=imgCanvas.getContext('2d');
+      imgCanvasctx.drawImage(img,0,0,img.width,img.height,0,0,imgWidth,imgHeight);
+      $('#img').remove();
+      imgCanvas.id="img";
+      document.getElementById('image').appendChild(imgCanvas);
+      img=document.getElementById("img");
+    
+    pieceType=randomPieceTypes(rows,cols);
       for(i=1; i<=rows; i++){
         for(j=1; j<=cols; j++){
           piece = document.createElement('canvas');
@@ -39,11 +69,14 @@ function start(file) {
           //случайное положение кусочка в пределах картинки
           piece.style.top=Math.round(0.7*screen.height*Math.random())+'px';
           piece.style.left=Math.round(0.8*screen.width*Math.random())+'px';
-          piece.width=colsWidth;
-          piece.height=rowsHeight;
+      mask = document.getElementById(pieceType[i-1][j-1]);
+          piece.width=puzzleSize;
+          piece.height=puzzleSize;
           piece.style.zIndex=2;
           piecectx=piece.getContext('2d');
-          piecectx.drawImage(img, (j-1)*colsWidth, (i-1)*rowsHeight, colsWidth, rowsHeight, 0, 0, colsWidth, rowsHeight);
+      piecectx.drawImage(mask,0,0,puzzleSize, puzzleSize);
+      piecectx.globalCompositeOperation='source-in';
+          piecectx.drawImage(img, (j-1)*puzzleLogical-puzzleOffset, (i-1)*puzzleLogical-puzzleOffset, puzzleSize, puzzleSize, 0, 0, puzzleSize, puzzleSize);
           document.getElementById('game').appendChild(piece);
           $('#piece'+i+'_'+j).data({'x':j-1,'y':i-1, angle:0}); //в куски записываем информацию об их "координатах"
           if (isRotate){
@@ -55,23 +88,42 @@ function start(file) {
         }
       }
       //уменьшение изображения и кусочков
-      img.height=$('#background').height();
-      rowsHeight = Math.round(img.height/rows);
-      colsWidth = Math.round(img.width/cols);
-      $('.piece').css('width',colsWidth);
+    $('#img').css('height',$('#background').height());
+    if ($('#img').width()>screen.width) {
+     $('#img').css('width',screen.width);
+     $('#img').css('height','');
+    }
+      imgleft=0;
+    imgtop=0;
+      if ($('#img').width()<$('#background').width()){
+    imgleft=($('#background').width()-$('#img').width())/2;
+    $('#img').css('left',imgleft);
+      }
+    if ($('#img').height()<$('#background').height()){
+    imgtop=($('#background').height()-$('#img').height())/2;
+    $('#img').css('top',imgtop);
+      }
+    puzzleLogical=Math.round($('#img').height()/rows);
+    puzzleOffset=puzzleLogical/100*35;
+    puzzleSize=puzzleLogical/100*170;
+      $('.piece').css('width',puzzleSize);
       //Перетаскивание
       $('.piece').draggable({});
       //подъём кусочка при нажатии
-      $('.piece').mousedown(function(){
+      $('.piece').mousedown(function(e){
+    if (!isPiece(this.id,e)) {
+      passEventLower(this.id, e);
+      return;
+    }
         if (!$(this).hasClass('piece')) return;
           $(this).css('z-index',zIndex); 
         zIndex++;
       });
       $('.piece').mouseup(function(){
         if (!$(this).hasClass('piece')) return;
-          if ((Math.abs($(this).data('x')*colsWidth-$(this).position().left)<=3) && (Math.abs($(this).data('y')*rowsHeight-$(this).position().top)<=3) && ($(this).data('angle')%360==0)){
+          if ((Math.abs($(this).data('x')*puzzleLogical-$(this).offset().left-$('#game').scrollLeft()-puzzleOffset)<=3+imgleft) && (Math.abs($(this).data('y')*puzzleLogical-$(this).offset().top-$('#game').scrollTop()-puzzleOffset)<=3+imgtop) && ($(this).data('angle')%360==0)){
               $(this).draggable('disable');
-              $(this).css({'top':$(this).data('y')*rowsHeight, 'left':$(this).data('x')*colsWidth,'z-index':1});
+              $(this).css({'top':$(this).data('y')*puzzleLogical+imgtop-puzzleOffset, 'left':$(this).data('x')*puzzleLogical+imgleft-puzzleOffset,'z-index':1,'transform':'rotate(0deg)'});
           $(this).removeClass('piece').addClass('solved'); //для удаления и присовения класса
       }
         //для условия решения
@@ -124,6 +176,8 @@ function restart(){
   $('#settings').show();
   $('#game').show();
   $("#sendRecord").show();
+  $('#img').remove();
+  $('#image').append('<img id="img">');
 }
 //отправка рекорда
 function send(){
@@ -132,4 +186,17 @@ function send(){
   //request.count.push();
   //request.time.push();
   $.ajax({url: "newrecord.php", data:{'name':$('#Name').val(),'count':rows*cols,'time':t}, async:true, success: function(hide){$("#sendRecord").hide();}});
+}
+
+function isPiece (id,e) {
+  var x = e.pageX-$('#'+id).offset().left;
+  var y = e.pageY-$('#'+id).offset().top;
+  return ((x>puzzleOffset)&&(x<puzzleSize-puzzleOffset)&&(y>puzzleOffset)&&(y<puzzleSize-puzzleOffset)) ? true : false;
+}
+function passEventLower(id, e) {
+  $('#'+id).hide();
+    var $el = $(document.elementFromPoint(e.pageX, e.pageY));
+  $('#'+id).trigger('mouseup');
+    $el.trigger(e);
+  $('#'+id).show();
 }
